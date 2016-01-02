25bfe8006b0fd4f61709826ae150883b60daf787
Noah Heinrich <noahheinrich@gmail.com>
2016-01-02 13:47:51 -0600

    Add control test

--- application.js
+++ application.js
@@ -1,26 +1,41 @@
 $(document).ready(function(){
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  var xMove = 5;
-  var yMove = 3;
   var x = canvas.width/3;
   var y = 300;
   var color = "#FF0000";
-  var draw = function(){
+  ctx.fillStyle = color;
+  ctx.fillRect(x,y,25,25);
+
+  $(document).on("keydown", function(event){
+    event.preventDefault();
+    var code = event.keyCode;
+    if(code===37){
+      draw("left")
+    } else if(code === 38){
+      draw("up")
+    } else if(code === 39){
+      draw("right")
+    } else if(code === 40){
+      draw("down")
+    }
+  })
+
+  var draw = function(direction){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
     ctx.fillStyle = color;
     ctx.fillRect(x,y,25,25);
     ctx.closePath();
-    if(y + yMove < 0 || y + yMove > canvas.height - 25){
-      yMove = -yMove;
+    if(x > 0 && direction === "left"){
+      x += -15;
+    } else if(y > 0 && direction === "up"){
+      y += -15;
+    } else if(x + 10 < canvas.width - 20 && direction === "right"){
+      x += 15
+    } else if(y + 10 < canvas.height - 20 && direction === "down"){
+      y += 15
     }
-    if(x + xMove < 0 || x + xMove > canvas.width - 25){
-      xMove = -xMove;
-    }
-    x += xMove;
-    y += yMove;
   }
-  setInterval(draw, 10);
 })
